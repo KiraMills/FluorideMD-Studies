b18ed67e92d25c98c993b2c3954f73ace43205d9
Kira Mills <57125498+KiraMills@users.noreply.github.com>
2023-11-17 14:29:39 -0600

    Update run.sh

--- Fluc_CompEL/MD_Input/run.sh
+++ Fluc_CompEL/MD_Input/run.sh
@@ -28,20 +28,20 @@ pmemd.cuda -O -i 05_Heat2.in -o 05_Heat2.out -p NAME.prmtop -c 04_Heat1.rst -r 0
 
 #Run the equilibration steps, for membranes/membrane proteins it's standard to do 10 x 500 ps steps, where each step has the run command: 
 #pmemd.cuda -O -i input_file -o output_file -p prmtop -c starting_coords -r restart_file -ref ref_coords_for_restraints -x trajectory_file
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold1.out -p NAME.prmtop -c 05_Heat2.rst -r 06_Hold1.rst -ref 05_Heat2.rst -x 06_Hold1.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold2.out -p NAME.prmtop -c 06_Hold1.rst -r 06_Hold2.rst -ref 06_Hold1.rst -x 06_Hold2.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold3.out -p NAME.prmtop -c 06_Hold2.rst -r 06_Hold3.rst -ref 06_Hold2.rst -x 06_Hold3.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold4.out -p NAME.prmtop -c 06_Hold3.rst -r 06_Hold4.rst -ref 06_Hold3.rst -x 06_Hold4.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold5.out -p NAME.prmtop -c 06_Hold4.rst -r 06_Hold5.rst -ref 06_Hold4.rst -x 06_Hold5.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold6.out -p NAME.prmtop -c 06_Hold5.rst -r 06_Hold6.rst -ref 06_Hold5.rst -x 06_Hold6.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold7.out -p NAME.prmtop -c 06_Hold6.rst -r 06_Hold7.rst -ref 06_Hold6.rst -x 06_Hold7.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold8.out -p NAME.prmtop -c 06_Hold7.rst -r 06_Hold8.rst -ref 06_Hold7.rst -x 06_Hold8.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold9.out -p NAME.prmtop -c 06_Hold8.rst -r 06_Hold9.rst -ref 06_Hold8.rst -x 06_Hold9.nc
-pmemd.cuda -O -i 06_Hold.in -o 06_Hold10.out -p NAME.prmtop -c 06_Hold9.rst -r 06_Hold10.rst -ref 06_Hold9.rst -x 06_Hold10.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil1.out -p NAME.prmtop -c 05_Heat2.rst -r 06_Equil1.rst -ref 05_Heat2.rst -x 06_Equil1.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil2.out -p NAME.prmtop -c 06_Equil1.rst -r 06_Equil2.rst -ref 06_Equil1.rst -x 06_Equil2.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil3.out -p NAME.prmtop -c 06_Equil2.rst -r 06_Equil3.rst -ref 06_Equil2.rst -x 06_Equil3.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil4.out -p NAME.prmtop -c 06_Equil3.rst -r 06_Equil4.rst -ref 06_Equil3.rst -x 06_Equil4.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil5.out -p NAME.prmtop -c 06_Equil4.rst -r 06_Equil5.rst -ref 06_Equil4.rst -x 06_Equil5.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil6.out -p NAME.prmtop -c 06_Equil5.rst -r 06_Equil6.rst -ref 06_Equil5.rst -x 06_Equil6.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil7.out -p NAME.prmtop -c 06_Equil6.rst -r 06_Equil7.rst -ref 06_Equil6.rst -x 06_Equil7.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil8.out -p NAME.prmtop -c 06_Equil7.rst -r 06_Equil8.rst -ref 06_Equil7.rst -x 06_Equil8.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil9.out -p NAME.prmtop -c 06_Equil8.rst -r 06_Equil9.rst -ref 06_Equil8.rst -x 06_Equil9.nc
+pmemd.cuda -O -i 06_Equil.in -o 06_Equil10.out -p NAME.prmtop -c 06_Equil9.rst -r 06_Equil10.rst -ref 06_Equil9.rst -x 06_Equil10.nc
 
 
 #Run the production steps, repeat this as many times as needed to get useful information, where each step has the run command: 
 #pmemd.cuda -O -i input_file -o output_file -p prmtop -c starting_coords -r restart_file -ref ref_coords_for_restraints -x trajectory_file
-pmemd.cuda -O -i 07_Prod.in -o 07_Prod1.out -p NAME.prmtop -c 06_Hold10.rst -r 07_Prod1.rst -ref 06_Hold10.rst -x 07_Prod1.nc
+pmemd.cuda -O -i 07_Prod.in -o 07_Prod1.out -p NAME.prmtop -c 06_Equil10.rst -r 07_Prod1.rst -ref 06_Equil10.rst -x 07_Prod1.nc
 pmemd.cuda -O -i 07_Prod.in -o 07_Prod2.out -p NAME.prmtop -c 07_Prod1.rst -r 07_Prod2.rst -ref 07_Prod1.rst -x 07_Prod2.nc
 pmemd.cuda -O -i 07_Prod.in -o 07_Prod3.out -p NAME.prmtop -c 07_Prod2.rst -r 07_Prod3.rst -ref 07_Prod2.rst -x 07_Prod3.nc
